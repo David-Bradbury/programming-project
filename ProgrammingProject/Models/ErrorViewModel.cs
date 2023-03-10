eb43b093a9698626c1f708eb68781a8eee2afa37
davidbradburyau <48012335+davidbradburyau@users.noreply.github.com>
2023-03-10 11:33:17 +1100

    Initial commit

---
+++ ProgrammingProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProgrammingProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
