d13d60c0266eefae50f68a5317767ddd7fff9cbb
davidp84 <62162575+davidp84@users.noreply.github.com>
2023-03-11 11:54:45 +1100

    Basic models creation

---
+++ ProgrammingProject/Models/Login.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProgrammingProject.Models
+{
+    public enum Locked
+    {
+        unlocked = 0,
+        locked = 1
+    }
+
+    public record Login
+    {
+        [Column(TypeName = "nchar")]
+        [StringLength(8)]
+        public string id { get; init; }
+
+        public virtual User User { get; init; }
+
+        [Column(TypeName = "nchar")]
+        [Required, StringLength(64)]
+        public string password { get; init; }
+
+        public Locked Locked { get; init; }
+    }
+}
