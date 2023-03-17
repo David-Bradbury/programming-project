62631cfee7543ef51e50b33e0125e5544b19c912
davidbradburyau <48012335+davidbradburyau@users.noreply.github.com>
2023-03-17 14:02:56 +1100

    Added Dog, Owner and Vet

---
+++ ProgrammingProject/Models/Dog.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProgrammingProject.Models
+{
+    public enum DogSize
+    {
+        Small = 0,
+        MediumSmall = 1,
+        MediumLarge = 2,
+        Large = 3
+    }
+    public class Dog
+    {
+        [Required]
+        public int ID { get; set; }
+
+        [Required, StringLength(50)]
+        public string Name { get; set; }
+
+        [StringLength(50)]
+        public string MicrochipNumber { get; set; }
+        public bool IsVaccinated { get; set; }  
+
+
+        public virtual Owner Owner { get; set; }
+        
+
+
+
+    }
+}
---
+++ ProgrammingProject/Models/Owner.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProgrammingProject.Models
+{
+    public class Owner
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required, StringLength(50)]
+        public string FirstName { get; set; }
+
+        [Required, StringLength(50)]
+        public string LastName { get; set; }
+
+        [Required, StringLength(50)]
+        public string Address { get; set; }
+
+        [Required, StringLength(50)]
+        public string PhNumber { get; set; }
+
+        [Required, StringLength(50)]
+        public string Email { get; set; }
+        public virtual List<Dog> Dogs { get; set; }
+        public virtual Login Login { get; set; }
+
+
+
+
+
+    }
+}
