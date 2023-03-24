d7d248e431905d102e31efe98c71901e2a1d52c8
davidp84 <62162575+davidp84@users.noreply.github.com>
2023-03-24 21:27:20 +1100

    Dog model comment. OwnerController creation + addDog module.

---
+++ ProgrammingProject/Controllers/OwnerController.cs
@@ -0,0 +1,79 @@
+﻿using ProgrammingProject.Data;
+using ProgrammingProject.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SimpleHashing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProgrammingProject.Controllers
+{
+    //Mask URL
+    [Route("/Owner")]
+    public class OwnerController : Controller
+    {
+        private readonly EasyWalkContext _context;
+
+        public LoginController(EasyWalkContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            //lazy loading
+            var owner = await _context.Owner.FindAsync(OwnerID);
+            return View(customer);
+        }
+
+        public async Task<IActionResult> Dogs(int id)
+        {
+            var dog = await _context.Dogs.FindAsync(id);
+            return View(dog);
+        }
+
+        // Add a dog to the owner
+        public async Task AddDog(DogSize size, Temperament temperament, int id, 
+                                 string name, string microchip, bool IsVaccinated, Owner owner, Vet vet)
+        {
+            var dog = await _context.Dogs.FindAsync(id);
+
+            if (dog != null)
+            {
+                owner.Dogs.Add(
+                new Dog
+                {
+                    DogSize = size,
+                    Temperament = temperament,
+                    ID = id,
+                    Name = name,
+                    MicrochipNumber = microchip,
+                    IsVaccinated = IsVaccinated,
+                    Owner = owner,
+                    Vet = vet
+                });
+
+                await _context.SaveChangesAsync();
+            }
+
+        }
+
+        public async Task MatchDogToWalker(int id)
+        {
+            var dog = await _context.Dogs.FindAsync(id);
+
+            if (dog != null)
+            {
+                
+
+                await _context.SaveChangesAsync();
+            }
+
+        }
+
+
+    }
+}
+}
--- ProgrammingProject/Models/Dog.cs
+++ ProgrammingProject/Models/Dog.cs
@@ -34,7 +34,11 @@ namespace ProgrammingProject.Models
         public virtual Owner Owner { get; set; }
         public virtual Vet Vet { get; set; }
 
-
+        // The enums need to be added as 'Dog' variables,
+        // or we could possibly use some logic to create
+        // a score for the dog and store that. But I think
+        // it would make sense to keep the individual
+        // properties incase the logic changes in the future.DP
 
 
     }
