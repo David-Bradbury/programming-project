8bc16abc92b1c5222fce4b1f02f09c3e875e8d46
s3775648 <s3775648@student.rmit.edu.au>
2023-03-29 21:54:50 +1030

    Add DatabaseRefinement Branch

--- ProgrammingProject/Controllers/LoginController.cs
+++ ProgrammingProject/Controllers/LoginController.cs
@@ -1,61 +1,61 @@
-﻿using ProgrammingProject.Data;
-using ProgrammingProject.Models;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using SimpleHashing;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿//using ProgrammingProject.Data;
+//using ProgrammingProject.Models;
+//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Mvc;
+//using SimpleHashing;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
 
-namespace ProgrammingProject.Controllers
-{
-    //Mask URL
-    [Route("/Login")]
-    public class LoginController : Controller
-    {
-        //private readonly EasyWalkContext _context;
+//namespace ProgrammingProject.Controllers
+//{
+//    //Mask URL
+//    [Route("/Login")]
+//    public class LoginController : Controller
+//    {
+//        private readonly EasyWalkContext _context;
 
-        //public LoginController(EasyWalkContext context)
-        //{
-        //    _context = context;
-        //}
-
-
-        //public IActionResult Login() => View();
-
-        ////attempt loing
-        //[HttpPost]
-        //public async Task<IActionResult> Login(string loginID, string password)
-        //{
-        //    //find login id
-        //    var login = await _context.Logins.FindAsync(loginID);
-
-        //    //attempt password check
-        //    if (login == null || !PBKDF2.Verify(login.PasswordHash, password))
-        //    {
-        //        ModelState.AddModelError("LoginFailure", "Login attempt failed, please try again");
-        //        return View(new Login { LoginID = loginID });
-        //    }
-
-        //    //Customer login
-        //    HttpContext.Session.SetInt32(nameof(User.id), login.id);
-        //    HttpContext.Session.SetString(nameof(User.user_name), login.User.user_name);
-
-        //    return RedirectToAction("Index", "User");
-        //}
-
-        //[Route("LoggingOut")]
-        //public IActionResult Logout()
-        //{
-        //    HttpContext.Session.Clear();
-
-        //    return RedirectToAction("Index", "Home");
-        //}
+//        public LoginController(EasyWalkContext context)
+//        {
+//            _context = context;
+//        }
 
 
+//        public IActionResult login() => View();
+
+//        //attempt loging
+//        [HttpPost]
+//        public async Task<IActionResult> login(string loginID, string password)
+//        {
+//            //find login id
+//            var login = await _context.Logins.FindAsync(loginID);
+
+//            //attempt password check
+//            if (login == null || !PBKDF2.Verify(login.PasswordHash, password))
+//            {
+//                ModelState.AddModelError("loginfailure", "login attempt failed, please try again");
+//                return View(new Login { LoginID = loginID });
+//            }
+
+//            //customer login
+//            HttpContext.Session.SetInt32(nameof(User.Id), login.UserId);
+//            HttpContext.Session.SetString(nameof(User.User_name), login.user.user_name);
+             
+//            return RedirectToAction("Index", "User");
+//        }
+
+//        [Route("loggingOut")] 
+//        public IActionResult logout()
+//        {
+//            HttpContext.Session.Clear();
+
+//            return RedirectToAction("Index", "Home");
+//        }
 
 
-    }
-}
+
+
+//    }
+//}
 
