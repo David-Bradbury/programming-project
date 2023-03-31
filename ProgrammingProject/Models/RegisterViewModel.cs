e0e8c23b56c6c15b713c79dc19d57b2897430c90
davidbradburyau <48012335+davidbradburyau@users.noreply.github.com>
2023-03-31 16:34:26 +1100

    View Model created

---
+++ ProgrammingProject/Models/RegisterViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace ProgrammingProject.Models
+{
+    public class RegisterViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        
+        [Required, EmailAddress(ErrorMessage = "The email address is required")]
+        public string Email { get; set; }
+
+        [Required, StringLength(200)]
+        public string StreetAddress { get; set; }
+        [Required, StringLength(100)]
+        public string State { get; set; }
+        [Required, StringLength(100)]
+        public string Country { get; set; }
+        [Required, StringLength(50)]
+        public string PhNumber { get; set; }
+
+        public string Suburb { get; set; }
+
+        public int Postcode { get; set; }
+   
+        public bool IsInsured { get; set; }
+        public int ExperienceLevel { get; set; }
+        public string Password { get; set; }
+        public int LoginID { get; set; }
+
+        
+        }
+}
+
+}
+}
