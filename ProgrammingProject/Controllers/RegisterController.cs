8bfc3ce8fd708f830144eb174e1a8530f6b87861
davidbradburyau <48012335+davidbradburyau@users.noreply.github.com>
2023-03-31 13:54:21 +1100

    change app settings to correct connection string

---
+++ ProgrammingProject/Controllers/RegisterController.cs
@@ -0,0 +1,26 @@
+﻿using ProgrammingProject.Data;
+using ProgrammingProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProgrammingProject.Controllers
+{
+
+    [Route("/Register")]
+    public class RegisterController : Controller
+    {
+        private readonly EasyWalkContext _context;
+
+        public RegisterController(EasyWalkContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Register() => View();
+
+        //[HttpPost]
+        //public Task<IActionResult> Register()
+        //{
+
+        //}
+    }
+}
