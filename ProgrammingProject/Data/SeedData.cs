9bb021714f55f4e517c5bed5fcca23ca6586eee1
davidp84 <62162575+davidp84@users.noreply.github.com>
2023-04-09 20:00:08 +1000

    Added more seed data info for walking sessions

--- ProgrammingProject/Data/SeedData.cs
+++ ProgrammingProject/Data/SeedData.cs
@@ -328,15 +328,23 @@ namespace ProgrammingProject.Data
 
             // seed walkingSessions
 
+            var dogs = new List<Dog>();
+            dogs.Add(dog4);
+            dogs.Add(dog3);
+
             var walk = new WalkingSession();
             walk.StartTime = DateTime.Now;
             walk.EndTime = DateTime.Now.AddHours(1);
             walk.WalkerID = 4;
+            walk.Walker = walker1;
+            walk.DogList = dogs;
 
             var walk1 = new WalkingSession();
-            walk.StartTime = DateTime.Now.AddHours(2);
-            walk.EndTime = DateTime.Now.AddHours(3);
-            walk.WalkerID = 4;
+            walk1.StartTime = DateTime.Now.AddHours(2);
+            walk1.EndTime = DateTime.Now.AddHours(3);
+            walk1.WalkerID = 4;
+            walk1.Walker = walker1;
+            walk1.DogList = dogs;
 
             context.WalkingSessions.Add(walk1);
 
