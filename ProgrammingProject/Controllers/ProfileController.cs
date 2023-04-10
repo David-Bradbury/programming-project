f030ae3da5288a810066049455b7db59d965b7c3
davidbradburyau <48012335+davidbradburyau@users.noreply.github.com>
2023-04-10 14:20:07 +1000

    Display profile view done

--- ProgrammingProject/Controllers/ProfileController.cs
+++ ProgrammingProject/Controllers/ProfileController.cs
@@ -12,7 +12,7 @@ namespace ProgrammingProject.Controllers
         private bool isOwner;
         private int UserID => HttpContext.Session.GetInt32(nameof(Owner.UserId)).Value;
 
-       
+
         [AuthorizeUser]
         public ProfileController(EasyWalkContext context)
         {
@@ -23,13 +23,58 @@ namespace ProgrammingProject.Controllers
         [AuthorizeUser]
         public async Task<IActionResult> Index()
         {
-            var walker = await _context.Walkers.FindAsync(UserID);
-            //return View(walker);
-            ViewBag.Walker = walker;
+            var o = new Owner();
+            o = await _context.Owners.FindAsync(UserID);
 
-            return View();
+            var w = new Walker();
+            w = await _context.Walkers.FindAsync(UserID);
+
+            var viewModel = new EditProfileViewModel();
+
+            //Check usertype and create viewModel
+            if(o== null)
+            {
+                //User is Walker
+                viewModel.UserType = typeof(Walker).Name;
+                viewModel.FirstName = w.FirstName;
+                viewModel.LastName = w.LastName;
+                viewModel.Email = w.Email;
+                viewModel.StreetAddress = w.StreetAddress;
+                viewModel.SuburbName = w.Suburb.SuburbName;
+                viewModel.Postcode = w.Suburb.SuburbName;
+                viewModel.State = w.State;
+                viewModel.Country = w.Country;
+                viewModel.PhNumber = w.PhNumber;
+                viewModel.IsInsured = w.IsInsured;
+                viewModel.ExperienceLevel = (int) w.ExperienceLevel;
+
+            }
+            else
+            {
+                //User is Owner
+                viewModel.UserType = typeof(Owner).Name;
+                viewModel.FirstName = o.FirstName;
+                viewModel.LastName = o.LastName;
+                viewModel.Email = o.Email;
+                viewModel.StreetAddress = o.StreetAddress;
+                viewModel.SuburbName = o.Suburb.SuburbName;
+                viewModel.Postcode = o.Suburb.SuburbName;
+                viewModel.State = o.State;
+                viewModel.Country = o.Country;
+                viewModel.PhNumber = o.PhNumber;
+                //default values for ExperienceLevel an IsInsured as are not used for owner.
+
+            }
+
+            return View(viewModel);
 
         }
+        //public async Task<IActionResult> EditProfile(int id)
+        //{
+        //    var user = await _context.Owners.FindAsync(UserID);
+        // //   var viewModel = ControllerHelper.BuildEditProfileViewModel(customer, id);
+        //    return View(viewModel);
+        //}
 
     }
 }
--- ProgrammingProject/Models/EditProfileViewModel.cs
+++ ProgrammingProject/Models/EditProfileViewModel.cs
@@ -4,17 +4,23 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ProgrammingProject.Models
 {
-    public class YourProfileViewModel
+    public class EditProfileViewModel
     {
-        public int AccountTypeSelection { get; set; }
+        public string UserType { get; set; }
+        public string Email { get; set; }
+
+        [Required, StringLength(200), Display(Name = "Street Address")]
+        public string SelectedField { get; set; }
+
         [Required, StringLength(50), Display(Name = "First Name")]
         public string FirstName { get; set; }
+
         [Required, StringLength(50), Display(Name = "Last Name")]
         public string LastName { get; set; }
-        [Required, EmailAddress(ErrorMessage = "An email address is required")]
-        public string Email { get; set; }
-        [Required, StringLength(200), Display(Name = "Street Address")]
+
+        [Display(Name = "Address")]
         public string StreetAddress { get; set; }
+
         [Required, StringLength(100), Display(Name = "Suburb")]
         public string SuburbName { get; set; }
 
@@ -22,23 +28,25 @@ namespace ProgrammingProject.Models
         [RegularExpression(@"(^0[289][0-9]{2}\s*$)|(^[1-9][0-9]{3}\s*$)",
     ErrorMessage = "Must be a valid Australia postcode with 4 digits")]
         public string Postcode { get; set; }
+
         [Required, StringLength(30)]
         public string State { get; set; }
+
         [Required, StringLength(100)]
         public string Country { get; set; }
+
         [Required, StringLength(25)]
         [RegularExpression(@"^(\+?\(61\)|\(\+?61\)|\+?61|(0[1-9])|0[1-9])?( ?-?[0-9]){7,9}$",
     ErrorMessage = "Must be an Australian phone number starting (Example 04XX XXX XXX")]
-        [Display(Name = "Phone Number")]
+        [Display(Name = "Mobile Phone Number")]
         public string PhNumber { get; set; }
-        [Display(Name = "Are you insured? Tick if yes")]
+
+
+        [Display(Name = "Insured")]
         public bool IsInsured { get; set; }
-        [Range(1, 4), Display(Name = "Choose between 1= Beginner, 4 = Advanced")]
+
+        [Range(1, 4), Display(Name = "Experience Level")]
         public int ExperienceLevel { get; set; }
-        [Required]
-        public string Password { get; set; }
-        [Compare("Password", ErrorMessage = "The password does not match")]
-        public string ConfirmPassword { get; set; }
 
     }
 }
