795176149dc3d7ad6a74a28755025faba78c8ffd
s3775648 <s3775648@student.rmit.edu.au>
2023-04-28 09:16:02 +0930

    working on continued refactoring

--- ProgrammingProject/Helper/ImageHelper.cs
+++ ProgrammingProject/Helper/ImageHelper.cs
@@ -27,5 +27,55 @@ namespace ProgrammingProject.Helper
             }
             return fileName;
         }
+        private string UploadFile(AddDogViewModel viewModel)
+        {
+            string fileName = null;
+            if (viewModel.DogImage != null)
+            {
+                string uploadDirectory = Path.Combine(_webHostEnvironment.WebRootPath, "img");
+                fileName = Guid.NewGuid().ToString() + "-" + viewModel.DogImage.FileName;
+                string filePath = Path.Combine(uploadDirectory, fileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    viewModel.DogImage.CopyTo(fileStream);
+                }
+            }
+            return fileName;
+        }
+
+        private string UploadFile(EditDogProfileViewModel viewModel)
+        {
+            string fileName = null;
+            if (viewModel.DogImage != null)
+            {
+                string uploadDirectory = Path.Combine(_webHostEnvironment.WebRootPath, "img");
+                fileName = Guid.NewGuid().ToString() + "-" + viewModel.DogImage.FileName;
+                string filePath = Path.Combine(uploadDirectory, fileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    viewModel.DogImage.CopyTo(fileStream);
+                }
+            }
+            return fileName;
+        }
+        private string UploadFile(object viewModel)
+        {
+            RegisterViewModel vm = new RegisterViewModel();
+            if (viewModel.GetType() == typeof(RegisterViewModel))              
+                vm = (RegisterViewModel)viewModel;
+
+            string fileName = null;
+            if (vm.ProfileImage != null)
+            {
+                string uploadDirectory = Path.Combine(_webHostEnvironment.WebRootPath, "img");
+                fileName = Guid.NewGuid().ToString() + "-" + vm.ProfileImage.FileName;
+                string filePath = Path.Combine(uploadDirectory, fileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    vm.ProfileImage.CopyTo(fileStream);
+                }
+            }
+            return fileName;
+        }
     }
 }
